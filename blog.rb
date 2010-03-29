6cdabdee011b14e6359b378d26f097b577846c70
Roger Braun <roger.braun@student.uni-tuebingen.de>
2010-03-29 22:55:11 +0200

    First commit

---
+++ blog.rb
@@ -0,0 +1,31 @@
+require "rubygems"
+require "sinatra"
+require "haml"
+require "dm-core"
+require "dm-timestamps"
+
+DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3:db.sqlite3")
+
+class Post
+  include DataMapper::Resource
+  property :id, Serial
+  property :created_at, DateTime
+  property :title, String, :length => 255
+  property :body, Text
+end
+
+Post.auto_migrate!
+
+get "/" do
+  @posts = Post.all(:order => :created_at.desc)
+  haml :index
+end
+
+get "/post/new" do
+  haml :new_post
+end
+
+post "/post/new" do
+  Post.create(params)
+  redirect "/"
+end
